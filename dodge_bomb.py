058647676eed4f9a57af43aa94a53de9433d9d21
c0b22065c3 <c0b22065c3@edu.teu.ac.jp>
2023-10-03 14:18:43 +0900

    Practice 1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+from random import randint as ran
 import sys
 import pygame as pg
 
@@ -12,6 +13,12 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
+
+    bomb = pg.Surface((20, 20))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
+    bomb.set_colorkey((0, 0, 0))
+    bomb_pos = [ran(0, WIDTH), ran(0, HEIGHT)]
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb, bomb_pos)
         pg.display.update()
         tmr += 1
         clock.tick(10)
